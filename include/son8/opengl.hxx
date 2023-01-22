d86d28014786d58f14db84b34a148130f26f68fc
Sonejght <sonejght@gmail.com>
2023-01-22 09:57:52 +0200

    fix opengl include dependency

--- include/son8/opengl.hxx
+++ include/son8/opengl.hxx
@@ -5,6 +5,7 @@
 
 # ifndef          SON8OPENGL_DEFINED
 # define          SON8OPENGL_DEFINED
+# define          SON8OPENGL_INCLUDE
 # define      SON8OPENGL_VERSION_1_1
 # define      SON8OPENGL_VERSION_1_5
 # define      SON8OPENGL_VERSION_2_1
@@ -12,11 +13,6 @@
 # define      SON8OPENGL_VERSION_4_3
 # define     SON8OPENGL_PROFILE_CORE
 # define SON8OPENGL_VERSION 0x0403CE
-#include            "opengl-1_1.hxx"
-#include            "opengl-1_5.hxx"
-#include            "opengl-2_1.hxx"
-#include            "opengl-3_3.hxx"
-#include            "opengl-4_3.hxx"
 #  endif       // SON8OPENGL_DEFINED
 
 namespace son8::opengl::types
@@ -136,3 +132,11 @@ namespace son8::opengl::enums
     };
 #endif
 }
+
+#ifdef SON8OPENGL_INCLUDE
+#include "opengl-1_1.hxx"
+#include "opengl-1_5.hxx"
+#include "opengl-2_1.hxx"
+#include "opengl-3_3.hxx"
+#include "opengl-4_3.hxx"
+#endif
